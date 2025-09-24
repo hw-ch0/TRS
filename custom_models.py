e60f8d1dd0a7ada5b47c29ef6414f7150e4febb0
JeiminJeon <jeimin@yonsei.ac.kr>
2025-09-25 00:00:26 +0900

    minor update

--- custom_models.py
+++ custom_models.py
@@ -13,15 +13,15 @@ class LambdaLayer(nn.Module):
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
-
     def forward(self, x):
         return self.lambd(x)
 
 def _weights_init(m):
     classname = m.__class__.__name__
-    # print(classname)
+    print(classname)
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d) or isinstance(m, QConv):
         nn.init.kaiming_normal_(m.weight)
+
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -45,7 +45,6 @@ class BasicBlock(nn.Module):
                      nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
                      nn.BatchNorm2d(self.expansion * planes)
                 )
-
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
@@ -75,7 +74,6 @@ class QuantBlock(nn.Module):
                      QConv(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False, args=args),
                      nn.BatchNorm2d(self.expansion * planes)
                 )
-
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
